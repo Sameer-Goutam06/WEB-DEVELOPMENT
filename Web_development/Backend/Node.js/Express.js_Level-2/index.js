26c8186d0278feb405798f7e0eb17fccdeb55efa
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2024-03-28 07:36:35 +0530

    commits

--- Web_development/Backend/Node.js/Express.js_Level-2/index.js
+++ Web_development/Backend/Node.js/Express.js_Level-2/index.js
@@ -1,7 +1,21 @@
 const express=require('express');
 const app = express();
-console.dir(app);
 let port =8080;
+//used to check the port number and to receive requests
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
+//text based requests are converted into objects and they are parsed into JSON objects
+app.use((req,res)=>{
+    console.log("request received");
+    let obj={
+        name:"apple",
+        color:"red"
+    }
+    res.send(obj.parse(JSON));
+});
+//routing is the selectiong of path ->
+//for traffic in a network or between or across multiple networks
+//github.com/Sameer-Goutam06 is sending a request to show my profile in particular network
+//app.use can send only one response for any route and it wont change
+//but we need to send responses on basis of routes choosen
