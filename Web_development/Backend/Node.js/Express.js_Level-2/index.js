807122296a9b204db7c16d071687fc9aeaa2b05f
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2024-03-31 09:55:42 +0530

    routing

--- Web_development/Backend/Node.js/Express.js_Level-2/index.js
+++ Web_development/Backend/Node.js/Express.js_Level-2/index.js
@@ -6,16 +6,41 @@ app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
 //text based requests are converted into objects and they are parsed into JSON objects
-app.use((req,res)=>{
-    console.log("request received");
-    let obj={
-        name:"apple",
-        color:"red"
-    }
-    res.send(obj.parse(JSON));
-});
+
+// app.use((req,res)=>{
+//     console.log("request received");
+//     let obj={
+//         name:"apple",
+//         color:"red"
+//     }
+//     res.send(obj.parse(JSON));
+// });
+
+
+
+
 //routing is the selectiong of path ->
 //for traffic in a network or between or across multiple networks
 //github.com/Sameer-Goutam06 is sending a request to show my profile in particular network
 //app.use can send only one response for any route and it wont change
 //but we need to send responses on basis of routes choosen
+
+
+//app.get can create a response for individual routes
+//we can also use it to send responses for invalid routes
+//root path response
+app.get("/",(req,res)=>{
+    res.send("root path")
+});
+//sample path 1 response
+app.get("/main",(req,res)=>{
+    res.send("main path");
+});
+//sample path 2 response
+app.get("/secondary",(req,res)=>{
+    res.send("secondary path");
+});
+//universal path response for invalid route responses
+app.get("*",(req,res)=>{
+    res.send("invalid path");
+});
