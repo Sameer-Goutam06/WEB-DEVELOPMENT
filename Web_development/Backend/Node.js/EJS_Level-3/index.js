fc84512fdf771906661c55369365244017c5a3d0
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2024-04-01 08:20:13 +0530

    running server outside folder

--- Web_development/Backend/Node.js/EJS_Level-3/index.js
+++ Web_development/Backend/Node.js/EJS_Level-3/index.js
@@ -1,9 +1,14 @@
 const express= require('express');
 const app = express();
+//to set path even outside the ejs directory to run app globally
+const path= require('path');
 const port=8080;
 //using of ejs which is already installed in express folder hence we need to use it using app.set()
 //view => templates
 app.set("view engine", "ejs");
+//setting path to run server globally and to avoid views loading from root directory when it is located in a leaf directory
+app.set("views",path.join(__dirname,"/views"));
+
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
@@ -21,3 +26,5 @@ app.listen(port,()=>{
 app.get('/', function(req, res){
     res.render('home.ejs');
 });
+
+//it is always recommended to run the following code only inside the directory conatining views folder
