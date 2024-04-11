6191a4ae355830fa20a126e87f395123315ed784
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2024-04-11 09:13:37 +0530

    REST ended

--- Web_development/Backend/Node.js/REST_Level-6/index.js
+++ Web_development/Backend/Node.js/REST_Level-6/index.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const app = express();
 port=8080;
+
 const path=require('path');
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed';//imported form npm website
 
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
@@ -9,6 +12,9 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 //styling and js path
 app.use(express.static(path.join(__dirname,"public")));
+
+const methodOverride =require("method-override");
+app.use(methodOverride('_method'));
 
 app.listen(port,()=>{
     console.log('listening on port: '+port);
@@ -20,22 +26,22 @@ app.get("/",function(req,res){
 
 let posts=[
     {
-        id:"1a",
+        id:uuidv4(),
         username:'Sameer',
         content:"Hello"
     },
     {
-        id:"2b",
+        id:uuidv4(),
         username:'Saketh',
         content:"I love coding"
     },
     {
-        id:"3c",
+        id:uuidv4(),
         username:'Sahith',
         content:"Embrace yourself"
     },
     {
-        id:"4b",
+        id:uuidv4(),
         username:'Sakshith',
         content:"Internship acquired"
     },
@@ -50,15 +56,40 @@ app.get("/posts/new",function(req,res){
 });
 
 app.post("/posts",function(req,res){
-    console.log(req.body);
-    posts.push(req.body);
+    let {username,content}=req.body;
+    let id=uuidv4();
+    posts.push({id,username,content});
     res.redirect("/posts");
 });
 
 app.get("/posts/:id",(req,res)=>{
     let {id}=req.params;
-    console.log(id);
-    if (id in posts.id){
-        res.send("request working");
-    }
-})
+    let post=posts.find((p)=>id===p.id);
+    res.render("show.ejs",{post});
+});
+
+//Creation of id for posts using UUID of Express
+//install it using npm i UUID
+
+//Implementing patch to update conetent of a specific post using patch() request
+app.patch("/posts/:id",(req, res)=>{
+    let {id}=req.params;
+    let newContent =req.body.content;
+    let post=posts.find((p)=>id===p.id)
+    post.content = newContent;
+    res.redirect("/posts");
+});
+
+//post editing route
+app.get("/posts/:id/edit",(req, res)=>{
+    let {id}=req.params;
+    res.render("edit.ejs");
+});
+
+//post deleting route
+//we are filtering that particular post from all posts
+app.delete("/posts/:id",(req, res)=>{
+    let {id}=req.params;
+    posts=posts.filter((p)=> id!==p.id);
+    res.redirect("/posts");
+});
