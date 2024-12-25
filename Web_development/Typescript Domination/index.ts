796f1a9972139b24778cc0610fa2df79ab0ce045
Sameer-Goutam06 <r.n.v.s.sgautham@gmail.com>
2024-12-25 11:29:59 +0530

    Arrays in TypeScript completed successfully

--- Web_development/Typescript Domination/index.ts
+++ Web_development/Typescript Domination/index.ts
@@ -1,2 +1,20 @@
-let x:number = 100;
+//you have a choice to declare a type or else typescript would automatically allote the type for variable declarations
+let x: number=1000;
 console.log(x);
+//OR
+let y=20;
+//let y: number given by ts compiler itself
+console.log(y);
+
+//primitive types(number, string,boolean,array)
+//arrays in typescript can store more than 2 datatype values
+//array data types would be declared itself if we don't declare it explicitly
+let arr1=[1,2,3,"sameer"];
+//arrays can even store JSON data types and gives the structures of JSON files that are used
+let arr2=[1,2,3,{name:"sameer",class:"json"}];
+//we can declare two or more data types in an array explicitly
+let arr3:(string|number)[]=[1,2,3,"sameer"];
+//we can even declare JSON data types in arrays explicitly and give the structure 
+let arr4:(number|{name:string})[]=[1,2,3,{name:"abc"}]
+console.log(arr1,arr2,arr3,arr4);
+
